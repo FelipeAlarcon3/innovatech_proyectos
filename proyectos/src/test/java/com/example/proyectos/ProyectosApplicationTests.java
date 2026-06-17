8b67315197e59a40b427b4699caec0628deee8a1
Felipe Alarcon <fe.alarconz@duocuc.cl>
2026-06-16 22:44:19 -0400

    mejore las pruebas unitarias

--- proyectos/src/test/java/com/example/proyectos/ProyectosApplicationTests.java
+++ proyectos/src/test/java/com/example/proyectos/ProyectosApplicationTests.java
@@ -1,13 +1,49 @@
 package com.example.proyectos;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.example.proyectos.Model.Proyecto;
+import com.example.proyectos.Repository.ProyectoRepository;
+import com.example.proyectos.Service.ProyectoService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProyectosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ProyectoRepository proyectoRepository;
 
+    @Autowired
+    private ProyectoService proyectoService;
+
+    @Test
+    void contextLoads() {
+
+    }
+
+    @Test
+    void testCalcularPorcentajeAvance_FlujoReal() {
+
+        Proyecto proyecto = new Proyecto();
+        proyecto.setNombre("Proyecto Test Cobertura");
+        proyecto.setEstado("ACTIVO");
+        proyecto.setTareasTotales(10);
+        proyecto.setTareasCompletadas(7);
+
+
+        Proyecto proyectoGuardado = proyectoRepository.save(proyecto);
+        Long idGenerado = proyectoGuardado.getId();
+
+
+        Proyecto resultado = proyectoService.buscarPorId(idGenerado).orElse(null);
+
+
+        assertNotNull(resultado);
+        assertEquals("Proyecto Test Cobertura", resultado.getNombre());
+
+
+        assertEquals(70, resultado.getAvancePorcentaje());
+    }
 }
